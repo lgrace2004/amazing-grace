5275e939e570652e829d96eaf68b3690ab7334e3
Lyn Grace <lyn.grace6@gmail.com>
2017-11-21 19:07:12 -0600

    added READme page

--- src/App.js
+++ src/App.js
@@ -8,11 +8,17 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Wellllcome to React</h1>
+          <img src= "https://ibb.co/erJXCm" alt="sorr-y"/>
+          <h1 className="App-title">Amazing Grace Honey</h1>
         </header>
+        <button>Home</button>
+        <button>About</button>
+        <button>Blog</button>
+        <button>Products</button>
+        <button>BeeCam</button>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
-          Amazing Grace Honey
+          
 
         </p>
       </div>
